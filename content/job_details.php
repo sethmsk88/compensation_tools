0c375eaa08f55987fac243a81b0daa35dd0fb679
Seth Kerr <sethmsk88@yahoo.com>
2016-01-07 10:24:58 -0500

    Create Job Details page

---
+++ content/job_details.php
@@ -0,0 +1,88 @@
+<link href="./css/job_details.css" rel="stylesheet">
+
+<br />
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-3">
+			<span class="myLabel">Job Code:</span>
+		</div>
+		<div class="col-md-9">
+			<span class="myLabel">Job Title:</span>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+			<span class="myLabel">Job Family:</span>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-3">
+			<span class="myLabel">Pay Plan:</span>
+		</div>
+		<div class="col-md-9">
+			<span class="myLabel">Pay Level:</span>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+			<span class="myLabel">FLSA Status:</span>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+			<span class="myLabel">Description:</span>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+			<span class="myLabel">Education/Experience:</span>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+			<span class="myLabel">Recommended Competencies:</span>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+			<table id="otherReq-table" class="table table-bordered table-condensed">
+				<thead>
+					<tr>
+						<th colspan="2">Other Specific Requirements</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Police Background Check</td>
+						<td>Test</td>
+					</tr>
+					<tr>
+						<td>Financial Disclosure</td>
+						<td>Test</td>
+					</tr>
+					<tr>
+						<td>Pre/Post Offer Physical</td>
+						<td>Test</td>
+					</tr>
+					<tr>
+						<td>Confidentiality Statement</td>
+						<td>Test</td>
+					</tr>
+					<tr>
+						<td>Child Care Security Check</td>
+						<td>Test</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+</div>
